efea079305de56332c089c628432bee3ba4b49e0
Shujie Ma <shujie1st@gmail.com>
2023-02-18 20:39:36 -0500

    Create function to take a sentence and display it as an animation

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    if (i === sentence.length - 1) {
+      process.stdout.write('\n');
+    }
+  }, 50 * i);
+}
